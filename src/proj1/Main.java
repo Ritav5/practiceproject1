3828f198fc5cf4986968642bc2e66a32dfeade48
Ritav5 <tld0205@163.com>
2019-02-16 20:08:49 +0800

    step1

---
+++ src/proj1/Main.java
@@ -0,0 +1,11 @@
+package proj1;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int i=0;
+        System.out.println("test");
+        System.out.println("test22");
+    }
+}
