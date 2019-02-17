7d624921fa64a6b18fd6caf275eb2fce86fb2655
Ritav5 <tld0205@163.com>
2019-02-17 17:32:32 +0800

    step1.2

--- src/proj1/Main.java
+++ src/proj1/Main.java
@@ -2,9 +2,16 @@ package proj1;
 
 public class Main {
 
+    public static void demo1()
+    {
+        Fengzhuang1 a = new Fengzhuang1("Jack",25);
+        a.play();
+        Fengzhuang1 jicheng = new Jicheng2("Jack",24,"male");
+        jicheng.play();
+    }
     public static void main(String[] args)
     {
-        System.out.println("test");
-        System.out.println("test22");
+        demo1();
+        //System.out.println("test");
     }
 }
